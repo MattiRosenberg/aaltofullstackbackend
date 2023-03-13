950a9e3e63b62447173e2fadc02183ff41d0989d
Matti Rosenberg <matti.rosenberg@vincit.fi>
2023-03-13 14:25:09 +0200

    Add frontend to repository

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
+app.use(express.static('build'))
+app.use(cors())
 
 morgan.token('body', function logBody(req) {
   return JSON.stringify(req.body);

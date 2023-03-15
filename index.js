07e0ac1f98255e1c2538ef7fb2112d314cd73baf
Matti Rosenberg <matti.rosenberg@vincit.fi>
2023-03-15 13:04:59 +0200

    Update other endpoint to support mongodb

--- index.js
+++ index.js
@@ -1,117 +1,113 @@
 const express = require('express');
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require('morgan');
+const cors = require('cors');
 
+require('dotenv').config();
+
+const Person = require('./models/person');
+const { response } = require('express');
 const app = express();
 
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
-app.use(express.static('build'))
-app.use(cors())
+app.use(express.static('build'));
 
 morgan.token('body', function logBody(req) {
   return JSON.stringify(req.body);
 });
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5325523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({})
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-  const person = persons.find((person) => person.id === id);
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      console.log(`Person with id:${req.params.id} removed`);
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.post('/api/persons', (req, res) => {
+  const newPerson = new Person({
+    name: req.body.name,
+    number: req.body.number,
+  });
+
+  console.log(newPerson);
+
+  newPerson.save().then((savedPerson) => {
+    res.json(newPerson);
+  });
+});
+
+app.put('/api/persons/:id', (req, res) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+  };
+
+  console.log(person);
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true }).then(
+    (updatedPerson) => {
+      res.json(updatedPerson);
+    }
+  );
 });
 
 app.get('/api/info', (req, res) => {
-  const info = `Phonebook has info for ${persons.length} people`;
-  const now = new Date();
-  const day = now.getDate();
-  const month = now.getMonth();
-  const year = now.getFullYear();
+  Person.find({}).then((result) => {
+    const info = `Phonebook has info for ${result.length} people`;
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
 
-  const time = `${day} ${month} ${year}`;
+    const time = `${day} ${month} ${year}`;
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`${info} \n ${time}`);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`${info} \n ${time}`);
+  });
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
-  console.log(`Person with id:${id} removed`);
-  res.status(204).end();
-});
-
-const getNewId = () => {
-  const generateId = () => {
-    return Math.floor(Math.random() * 1000);
-  };
-
-  let randomId = generateId();
-  while (persons.find((person) => person.id === randomId)) {
-    randomId = generateId();
-  }
-
-  return randomId;
-};
-
-const hasNameAndNumber = (newPerson) => {
-  return newPerson.name && newPerson.number;
-};
-
-const personExists = (newPerson) => {
-  return persons.find((person) => newPerson.name === person.name);
-};
-
-app.post('/api/persons', (req, res) => {
-  const person = req.body;
-  if (!hasNameAndNumber((newPerson = person))) {
-    return res.status(400).json({ error: 'name or number missing' });
-  }
-
-  if (personExists((newPerson = person))) {
-    return res.status(400).json({ error: 'person exist' });
-  }
-
-  person.id = getNewId();
-  persons = [...persons, person];
-
-  res.json(person);
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);

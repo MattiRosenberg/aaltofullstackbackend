f332b119d98eda38645db592123dd5d6bb27d8e3
Matti Rosenberg <matti.rosenberg@vincit.fi>
2023-05-15 12:31:07 +0300

    6.9 ready

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,34 +1,33 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector} from 'react-redux';
+import Filter from './components/Filter';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => {
+      return state.anecdotes.filter((anecdote) =>
+      anecdote.content.toString().includes(state.filter.content)
+    );
+  });
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    console.log('vote', id);
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
+      ))}
+      <h2>Filter</h2>
+      <Filter />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
